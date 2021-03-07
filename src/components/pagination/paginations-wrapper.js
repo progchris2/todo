890450a1fcba1495715b158bebd34da4f7a0f6cc
Gilles Chris <progch@gmail.com>
2021-03-07 21:07:56 +0100

    modify pagination

--- src/components/pagination/paginations-wrapper.js
+++ src/components/pagination/paginations-wrapper.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const PaginationsWrapper = ({ children }) => {
-  console.log(React.Children.toArray(children));
+  React.Children.toArray(children).map((v) => console.log(v.props));
+  //console.log(React.Children.toArray(children));
   return <div>{children}</div>;
 };
--- src/features/todo/todo-list.js
+++ src/features/todo/todo-list.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { usePaginate } from "../../services/hooks/usePaginate";
 import { todoListProps } from "./definitions";
-import { PaginationsWrapper } from "../../components/pagination/paginations";
 import { Todo } from "../../components/todo";
+import { PaginationsWrapper } from "../../components/pagination/paginations-wrapper";
+import { CountPaginations } from "../../components/pagination/count-paginations";
 
 export const TodoList = ({ todos }) => {
+  const [index, setIndex] = useState(0);
   const { paginate } = usePaginate(todos);
+
+  if (!paginate.items.length) return null;
+
+  const showPaginateElement = () =>
+    paginate.items[index].map((item, k) => <Todo key={k} title="pagination" />);
+
+  const showSizePaginate = () =>
+    [...new Array(paginate.size)].map((item, k) => (
+      <CountPaginations key={k} />
+    ));
 
   return (
     <PaginationsWrapper>
-      <Todo />
-      fjkfgkndnvkjdgvn
+      {showPaginateElement()}
+      {showSizePaginate()}
     </PaginationsWrapper>
   );
 };
